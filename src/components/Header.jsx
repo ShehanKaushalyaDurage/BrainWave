0566cc4ab363171f0afb70f83b6e445747a1ac74
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-17 19:36:07 +0530

    header lg done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,49 @@
-import {brainwave} from "../assets";
+import { useLocation } from "react-router-dom";
+import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import Button from "./Button";
 
 const Header = () => {
+  const pathname = useLocation();
   return (
-   <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-    <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8 ">
-            <img src={brainwave} alt="brainwave" width={190} height={40}/>
+          <img src={brainwave} alt="brainwave" width={190} height={40} />
         </a>
 
         <nav className="hidden fixed top-[5rem] right-0 left-0 bottom-0 bg-n-8 lg:static lg:mx-auto lg:flex lg:bg-transparent">
-            <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-
-            </div>
-
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase transition-colors text-n-1 hover:text-color-1
+              ${
+                item.onlyMobile ? "lg:hidden" : ""
+              } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
         </nav>
+        <a
+          href="#signup"
+          className="text-n-1/50 mr-8 transition-colors button hidden hover:text-n-1 lg:block "
+        >
+          New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign In
+        </Button>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
