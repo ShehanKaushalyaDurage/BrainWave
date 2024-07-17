db2689c987f6b2a78550c15e9931c5077784c333
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-17 20:06:53 +0530

    header section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,10 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-
   const spanClasses = "relative z-10";
+
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
@@ -15,13 +15,12 @@ const Button = ({ className, href, onClick, children, px, white }) => {
 
   const renderLink = () => (
     <a href={href} className={classes}>
-        <span className={spanClasses}>{children}</span>
-        {ButtonSvg(white)}
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
     </a>
-  )
-  return href 
-  ? renderLink()
-  :renderButton();
+  );
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
