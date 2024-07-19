3ee8989e906158e90eac78f8344bc760cd7e330d
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-19 15:24:28 +0530

    img

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,4 @@
-import { check, service1 } from "../assets";
+import { check, service1, service2 } from "../assets";
 import { brainwaveServices } from "../constants";
 import Generating from "./Generating";
 import Heading from "./Heading";
@@ -30,14 +30,24 @@ const Services = () => {
               </p>
               <ul className="body-2 ">
                 {brainwaveServices.map((item, index) => (
-                  <li key={index} className="flex items-start py-4 border-t border-n-6">
-                    <img src={check} alt="" width={24} height={24}/>
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img src={check} alt="" width={24} height={24} />
                     <p className="ml-4">{item}</p>
                   </li>
                 ))}
               </ul>
             </div>
-            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2"/>
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl  overflow-hidden">
+              <div className="absolute inset-0 ">
+                <img src={service2} alt="Robot" className="h-full w-full object-cover" width={630} height={750}/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
