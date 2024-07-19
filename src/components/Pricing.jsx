68bd5427699984b174efccd262b387275754abf5
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-19 16:36:38 +0530

    see full detail part done

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -26,18 +26,20 @@ const Pricing = () => {
             />
           </div>
         </div>
-       
-          <Heading
-            tag="Get started with Brainwave"
-            title="Pay once, use forever"
-          />
 
-          <div className="relative ">
-            <PricingList/>
-            <LeftLine/>
-            <RightLine/>
-          </div>
-       
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+
+        <div className="relative ">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a className="text-xs font-bold font-code tracking-wider uppercase border-b " href="/pricing">See the full details</a>
+        </div>
       </div>
     </Section>
   );
