25352713263d03aeed2933d48489e54cc5121833
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-19 16:04:13 +0530

    Pricing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Benifits/>
         <Collaboration/>
         <Services/>
+        <Pricing/>
       </div>
       <ButtonGradient />
     </>
