725b860965db2d20abff3a6863b9e797b9e2bafa
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-04 00:36:36 +0530

    done

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -11,10 +11,10 @@ const Roadmap = () => {
     <Section className="overflow-hidden" id="roadmap">
       <div className="container md:pb-10">
         <Heading tag="Ready to get started" title="What we’re working on" />
-
         <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
           {roadmap.map((item) => {
             const status = item.status === "done" ? "Done" : "In progress";
+
             return (
               <div
                 className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
@@ -25,11 +25,11 @@ const Roadmap = () => {
                 <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
                   <div className="absolute top-0 left-0 max-w-full">
                     <img
-                      src={grid}
-                      alt="Grid"
                       className="w-full"
+                      src={grid}
                       width={550}
                       height={550}
+                      alt="Grid"
                     />
                   </div>
                   <div className="relative z-1">
@@ -46,13 +46,12 @@ const Roadmap = () => {
                         <div className="tagline">{status}</div>
                       </div>
                     </div>
-
                     <div className="mb-10 -my-10 -mx-15">
                       <img
-                        src={item.imageUrl}
                         className="w-full"
-                        width={630}
-                        height={420}
+                        src={item.imageUrl}
+                        width={628}
+                        height={426}
                         alt={item.title}
                       />
                     </div>
@@ -62,12 +61,13 @@ const Roadmap = () => {
                 </div>
               </div>
             );
+            
           })}
-          <Gradient />
+           
         </div>
         <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-          <Button href="/roadmap">Our roadmap</Button>
-        </div>
+        <Button href="/roadmap">Our roadmap</Button>
+      </div>
       </div>
     </Section>
   );
