4be9cc495be3ff66946fe5076da7933b5e3e28b4
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-03 23:31:32 +0530

    grid

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,19 +1,42 @@
-import Heading from './Heading'
-import Section from './Section'
+import { grid } from "../assets";
+import { roadmap } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
 
 const Roadmap = () => {
   return (
-   <Section className="overflow-hidden" id="roadmap">
-    <div className='container md:pb-10'>
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
         <Heading tag="Ready to get started" title="What we’re working on" />
 
-        <div>
-            
+        <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {roadmap.map((item) => {
+            const status = item.status === "done" ? "Done" : "In progress";
+            return (
+              <div key={item.id}>
+                <div>
+                  <div>
+                    <img
+                      src={grid}
+                      alt="Grid"
+                      className="w-full"
+                      width={550}
+                      height={550}
+                    />
+                  </div>
+                  <div className="relative z-1">
+                    <div>
+                        
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    </div>
+      </div>
+    </Section>
+  );
+};
 
-   </Section>
-  )
-}
-
-export default Roadmap
+export default Roadmap;
