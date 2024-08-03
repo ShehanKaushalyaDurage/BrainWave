aa3b60d5835d5bc14ce743363d4cb8e0e9164e26
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-03 23:24:39 +0530

    add header

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,19 @@
+import Heading from './Heading'
+import Section from './Section'
+
+const Roadmap = () => {
+  return (
+   <Section className="overflow-hidden" id="roadmap">
+    <div className='container md:pb-10'>
+        <Heading tag="Ready to get started" title="What we’re working on" />
+
+        <div>
+            
+        </div>
+    </div>
+
+   </Section>
+  )
+}
+
+export default Roadmap
