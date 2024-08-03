1451cd94e66b8a18b42d5bcd886faf2e7211debb
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-04 00:06:17 +0530

    roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,5 +1,7 @@
 import { check2, grid, loading1 } from "../assets";
 import { roadmap } from "../constants";
+import Button from "./Button";
+import { Gradient } from "./design/Hero";
 import Heading from "./Heading";
 import Section from "./Section";
 import TagLine from "./Tagline";
@@ -15,13 +17,13 @@ const Roadmap = () => {
             const status = item.status === "done" ? "Done" : "In progress";
             return (
               <div
-                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem]  ${
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
                   item.colorful ? "bg-conic-gradient" : "bg-n-6"
                 }`}
                 key={item.id}
               >
-                <div className="relative ">
-                  <div>
+                <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                  <div className="absolute top-0 left-0 max-w-full">
                     <img
                       src={grid}
                       alt="Grid"
@@ -31,10 +33,11 @@ const Roadmap = () => {
                     />
                   </div>
                   <div className="relative z-1">
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
                       <TagLine>{item.date}</TagLine>
-                      <div>
+                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
                         <img
+                          className="mr-2.5"
                           src={item.status === "done" ? check2 : loading1}
                           width={16}
                           height={16}
@@ -42,23 +45,28 @@ const Roadmap = () => {
                         />
                         <div className="tagline">{status}</div>
                       </div>
-                      <div className="mb-10 ">
-                        <img
-                          src={item.imageUrl}
-                          className="w-full"
-                          width={630}
-                          height={420}
-                          alt={item.title}
-                        />
-                      </div>
-                      <h4 className="h4 mb-4">{item.title}</h4>
-                      <p className="body-2 text-n-4">{item.text}</p>
                     </div>
+
+                    <div className="mb-10 -my-10 -mx-15">
+                      <img
+                        src={item.imageUrl}
+                        className="w-full"
+                        width={630}
+                        height={420}
+                        alt={item.title}
+                      />
+                    </div>
+                    <h4 className="h4 mb-4">{item.title}</h4>
+                    <p className="body-2 text-n-4">{item.text}</p>
                   </div>
                 </div>
               </div>
             );
           })}
+          <Gradient />
+        </div>
+        <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
+          <Button href="/roadmap">Our roadmap</Button>
         </div>
       </div>
     </Section>
